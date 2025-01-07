dccee4842531224288d1ba8262db1f8cdfcdfc3b
Akshay-dot123 <akshaytest123@gmail.com>
2025-01-07 09:35:24 +0530

    comments and day.js file added

--- day.js
+++ day.js
@@ -1,1 +1,12 @@
-const dayjs=require('dayjs')
+const dayjs = require("dayjs");
+const toArray = require("dayjs/plugin/toArray");
+dayjs.extend(toArray);
+const c = dayjs();
+console.log(c.format());
+console.log(c.hour(), c.minute(), c.second(), c.day());
+console.log(c.toArray());
+console.log(c.startOf("month").format("DD-MM-YYYY")); // Fomrat should be capital letters otherwise won't work
+
+const day=dayjs("2000-12-11")
+console.log(day.diff(c,"year"));  // Difference in day or month or year
+
--- multer.js
+++ multer.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const multer = require("multer");
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 const upload = multer({ dest: "uploads/" });
 
 const app = express();
-const helmet=require("helmet")
-app.use(helmet())
+const helmet = require("helmet");
+app.use(helmet());
 // Middleware to parse form data (this should be before the file upload middleware)
 // app.use(express.urlencoded({ extended: true }));
 
-// Upload only single file
+// Upload only single file with avatar field otherwise gives error
 // app.post("/profile", upload.single("avatar"), function (req, res, next) {
 //   try {
 //     console.log("File uploaded:", req.file);
@@ -21,25 +21,30 @@ app.use(helmet())
 //   }
 // });
 
-// Upload more then 1 file
-// app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
-//   try {
-//     console.log("File uploaded:", req.files);
-//     console.log("Username:", req.body.username);
-//     res.send(req.files);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// })
+// Upload more then 1 file with photos field otherwise gives error
+app.post(
+  "/photos/upload",
+  upload.array("photos", 12),
+  function (req, res, next) {
+    try {
+      console.log("File uploaded:", req.files);
+      console.log("Username:", req.body.username);
+      res.send(req.files);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
-const cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }])
-app.get('/cool-profile', cpUpload, function (req, res, next) {
-  if (req.files['avatar'] && req.files['avatar'].length > 1) {
-    return res.status(400).json({ message: 'Only one avatar file is allowed.' });
-}
+const cpUpload = upload.fields([{ name: "avatar", maxCount: 1 }]);
+app.get("/cool-profile", cpUpload, function (req, res, next) {
+  // If consition does not work because middleware handles error before executing below code
+  //   if (req.files['avatar'] && req.files['avatar'].length > 1) {
+  //     return res.status(400).json({ message: 'Only one avatar file is allowed.' });
+  // }
   console.log("Avatar file:", req.files.avatar); // Single file, array with 1 element
-  console.log("Gallery files:", req.files.gallery); 
-})
+  console.log("Gallery files:", req.files.gallery);
+});
 
 // In case you need to handle a text-only multipart form, you should use the .none() method:
 // app.post('/profile', upload.none(), function (req, res, next) {
@@ -50,7 +55,6 @@ app.get('/cool-profile', cpUpload, function (req, res, next) {
 //   }
 //   res.send("Correct")
 // })
-
 
 // const uploadDir = path.join(__dirname, 'uploads');
 // // Ensure the upload directory exists (if not, create it)
@@ -74,17 +78,16 @@ app.get('/cool-profile', cpUpload, function (req, res, next) {
 //   res.send("File uploaded successfully!");
 // });
 
-
 app.use((err, req, res, next) => {
   if (err instanceof multer.MulterError) {
     // Handle Multer-specific errors
     return res.status(400).json({
-      message: `Multer Error: ${err.message}`
+      message: `Multer Error: ${err.message}`,
     });
   } else {
     // Handle other errors
     return res.status(500).json({
-      message: `Internal Server Error: ${err.message}`
+      message: `Internal Server Error: ${err.message}`,
     });
   }
 });
